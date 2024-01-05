397799f9afd3541920ad812ab300ed5c39d9b7ee
hirano-vm4 <hirano.vm4@gmail.com>
2024-01-05 23:50:45 +0900

    Modified app.js to Only Execute the Application.

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
 #!/usr/bin/env node
 
 import { MyCryptoApp } from "./src/mycrypto_app.js";
-import { MyCryptoOutputFormat } from "./src/mycrypto_output_format.js";
 
-async function main() {
-  const prompt = new MyCryptoApp();
-  const cryptoCurrency = new MyCryptoOutputFormat();
-
-  try {
-    const userSelect = await prompt.getCryptoDisplayChoice();
-    const id = await prompt.fetchIdForSelectedTicker(userSelect);
-    const response = await prompt.cryptoMarket.getCryptoInfo(id);
-
-    if (id === 0) {
-      cryptoCurrency.displayMarketOverview(response);
-    } else {
-      cryptoCurrency.displayCurrencyDetails(response);
-    }
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(err.message);
-    } else {
-      throw err;
-    }
-  }
-}
-
-await main();
+const mycryptoApp = new MyCryptoApp();
+mycryptoApp.exec();
